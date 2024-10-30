c6b0037a348b7c5557358209198396d37198da1a
Pranav Anand Prakash Sharma <130064033+Pranav2092@users.noreply.github.com>
2024-10-30 12:55:09 +0530

    Initial Commit

---
+++ FlaskDemo.sql
@@ -0,0 +1,8 @@
+-- To create database and tables for project
+create database FlaskDemo;
+use FlaskDemo;
+create table Contacts(name varchar(200), phone_number varchar(15));
+
+-- To view Contents of Table
+select *
+from Contacts;
